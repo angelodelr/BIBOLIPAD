2146e55c25e71bdcb2dd932f4911230319ac4e68
angelodelr <150880099+angelodelr@users.noreply.github.com>
2025-11-11 00:06:05 +0800

    Update CameraFollow.cs

--- Main Map/Main Camera/CameraFollow.cs
+++ Main Map/Main Camera/CameraFollow.cs
@@ -1,1 +1,17 @@
+using UnityEngine;
 
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothTime = 0.08f;
+    public Vector3 offset = new Vector3(0f, 0f, -10f);
+    Vector3 velocity = Vector3.zero;
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+        Vector3 desired = target.position + offset;
+        Vector3 smoothed = Vector3.SmoothDamp(transform.position, desired, ref velocity, smoothTime);
+        transform.position = smoothed;
+    }
+}
