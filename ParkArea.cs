1098f7fa8e5b852b946981629768687cbe8ecfa9
angelodelr <150880099+angelodelr@users.noreply.github.com>
2025-11-10 23:56:47 +0800

    Add files via upload

---
+++ ParkArea.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class ParkArea : MonoBehaviour
+{
+    private BoxCollider2D box;
+
+    void Awake()
+    {
+        box = GetComponent<BoxCollider2D>();
+    }
+
+    // Returns the exact world-space rectangle for spawning
+    public Rect GetWorldRect()
+    {
+        Vector3 worldCenter = box.transform.TransformPoint(box.offset);
+        Vector3 worldSize = Vector3.Scale(box.size, box.transform.lossyScale);
+        return new Rect(
+            worldCenter.x - worldSize.x / 2f,
+            worldCenter.y - worldSize.y / 2f,
+            worldSize.x,
+            worldSize.y
+        );
+    }
+}
